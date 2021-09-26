a313a754c20462bfcf663d50edd47a494c43ee87
nhanhlen1 <91404191+nhanhlen1@users.noreply.github.com>
2021-09-26 13:02:17 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Điều đặc biệt to to",
+    introTitle: 'Cún àaaaaaa',
+    introDesc: 'Ngồi lại đây mình nói nhỏ với Cún một xíu nèeee',
+    btnIntro: 'Khoảng thời gian vừa qua Lu đã rất vui vì luôn có Cún ở bên cạnh 😘😘',
+    title: 'Nhưng mà Lu hơi ích kỉ một xíu, Lu chỉ muốn Cún là của riêng Lu thôi 🥺',
+    desc: 'Nên là... Cún làm bạn gái Lu nhéeee ',
+    btnYes: 'Được nè <3',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Vì đơn giản là Lu đẹp traiiiiiii :3 ',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Từ nay bạn là của mình rồi á :3 Mình sẽ chịu trách nhiệm với bạn nhaaa. Nếu mà nhớ mình thì nhắn tin cho mình nhé <3 Mình luôn ở đâyyyy ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100038845686262' 
+}
